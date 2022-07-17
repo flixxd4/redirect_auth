f52031245910565c8b2da7fc4ffc96059de33d6f
flixxd4 <97230549+flixxd4@users.noreply.github.com>
2022-07-17 21:26:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+function create_random_generator(string_length){
+    var random_string = ''
+    var char = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var i
+    
+    for(i = 0; i < string_length; i++){
+        random_string = random_string + char.charAt(Math.floor(Math.random()* char.length))
+    }
+    return random_string
+}
 
+var Random_key = create_random_generator(10)
+
+
+const fs = require('fs')
+fs.writeFile('output.txt', Random_key, (err) => {
+ if (err) throw err;
+});
